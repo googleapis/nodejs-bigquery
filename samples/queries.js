3fa804ca79f13f2d5b35ed6fdf1ff08a105939dd
Stephen Sawchuk <stephenplusplusplus@gmail.com>
2017-12-12 22:03:55 -0500

    Prettify.

--- samples/queries.js
+++ samples/queries.js
@@ -47,12 +47,15 @@ function queryStackOverflow(projectId) {
 
   // [START bigquery_simple_app_query]
   // The SQL query to run
-  const sqlQuery = `SELECT
+  const sqlQuery =
+    `SELECT
     CONCAT(
       'https://stackoverflow.com/questions/',
       CAST(id as STRING)) as url,
     view_count
-    FROM ` + '`bigquery-public-data.stackoverflow.posts_questions`' + `
+    FROM ` +
+    '`bigquery-public-data.stackoverflow.posts_questions`' +
+    `
     WHERE tags like '%google-bigquery%'
     ORDER BY view_count DESC
     LIMIT 10`;
