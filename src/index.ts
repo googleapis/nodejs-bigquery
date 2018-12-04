1cf49c3613b7207f0e3a01c8931a07a7384ebdfd
Christopher Wilcox <crwilcox@google.com>
2018-12-03 16:19:05 -0800

    docs: Improve timestamp documentation. (#280)

--- src/index.ts
+++ src/index.ts
@@ -573,7 +573,7 @@ export class BigQuery extends common.Service {
    * zone or convention such as Daylight Savings Time.
    *
    * @method BigQuery.timestamp
-   * @param {date} value The time.
+   * @param {Date|string} value The time.
    *
    * @example
    * const {BigQuery} = require('@google-cloud/bigquery');
@@ -585,7 +585,7 @@ export class BigQuery extends common.Service {
    * zone or convention such as Daylight Savings Time.
    *
    * @method BigQuery#timestamp
-   * @param {date} value The time.
+   * @param {Date|string} value The time.
    *
    * @example
    * const {BigQuery} = require('@google-cloud/bigquery');
