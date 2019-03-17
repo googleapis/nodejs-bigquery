179d43043f457a5442f5ae80708f43913abf950a
Steffany Brown <30247553+steffnay@users.noreply.github.com>
2019-03-17 06:37:54 -0700

    Update tables.test.js

--- samples/test/tables.test.js
+++ samples/test/tables.test.js
@@ -250,7 +250,7 @@ describe('Tables', () => {
   it(`should load a GCS ORC file truncate table`, async () => {
     const tableId = generateUuid();
     const output = await exec(
-      `node loadORCFromGCSTruncate.js ${datasetId} ${tableId}`
+      `node loadOrcFromGCSTruncate.js ${datasetId} ${tableId}`
     );
     assert.match(output, /completed\./);
     const [rows] = await bigquery
