0fbd26f310ff61b174d368bdb29596af38b0522a
Dave Gramlich <callmehiphop@gmail.com>
2019-05-21 16:47:40 -0400

    export model class

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,7 @@ import * as uuid from 'uuid';
 
 import {Dataset, DatasetOptions} from './dataset';
 import {Job, JobOptions, QueryResultsOptions} from './job';
+import {Model} from './model';
 import {
   Table,
   TableField,
@@ -1714,6 +1715,15 @@ export {Dataset};
  * @type {constructor}
  */
 export {Job};
+
+/**
+ * {@link Model} class.
+ *
+ * @name BigQuery.Model
+ * @see Model
+ * @type {constructor}
+ */
+export {Model};
 
 /**
  * {@link Table} class.
