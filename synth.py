a4e015ac599e0c125a64bf49dc77af062ed3edb4
Dave Gramlich <callmehiphop@gmail.com>
2019-09-10 13:25:52 -0400

    build: generate discovery types via synthtool

--- synth.py
+++ synth.py
@@ -6,3 +6,6 @@ logging.basicConfig(level=logging.DEBUG)
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
+
+# Node.js specific cleanup
+subprocess.run(['npm', 'run', 'types'])
