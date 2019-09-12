7f711ed473f66e1b793e80f354852cfb95fc6eef
Dave Gramlich <callmehiphop@gmail.com>
2019-09-12 09:38:56 -0400

    build: add npm install command to synth file (#538)

--- synth.py
+++ synth.py
@@ -9,4 +9,6 @@ templates = common_templates.node_library(source_location='build/src')
 s.copy(templates)
 
 # Node.js specific cleanup
+subprocess.run(['npm', 'install'])
 subprocess.run(['npm', 'run', 'types'])
+subprocess.run(['npm', 'run', 'fix'])
