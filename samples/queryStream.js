b2f25b936cd7b42bbe4695c44d4a6c026b42c2cd
Alejandro Soto <alesoto9113@gmail.com>
2020-05-09 03:13:16 -0400

    Fix

--- samples/queryStream.js
+++ samples/queryStream.js
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -40,8 +40,8 @@ function main() {
     })
     .on('data', row => {
       console.log('Row:', row);
-    }
-    ).on('end', () => {
+    })
+    .on('end', () => {
       console.log('End');
     });
   }
