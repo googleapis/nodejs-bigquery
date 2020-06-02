5ba4c030df8289292d1407ceaede2f7c3d426677
Lalji Kanjareeya <46327204+laljikanjareeya@users.noreply.github.com>
2020-06-02 07:43:34 +0530

    fix: Update src/table.ts
    Co-authored-by: Stephen <stephenplusplus@users.noreply.github.com>

--- src/table.ts
+++ src/table.ts
@@ -563,6 +563,7 @@ class Table extends common.ServiceObject {
    *     metadata object should be in the format of a
    *     [`JobConfigurationTableCopy`](https://cloud.google.com/bigquery/docs/reference/rest/v2/Job#JobConfigurationTableCopy)
    * object.
+   *     object.
    * @param {string} [metadata.jobId] Custom id for the underlying job.
    * @param {string} [metadata.jobPrefix] Prefix to apply to the underlying job
    *     id.
