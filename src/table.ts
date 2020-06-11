b2805ae4e6bcd2c74d60854438f6da8f71ac7c24
Lalji Kanjareeya <lalji.kanjareeya@qlogic.io>
2020-06-11 19:05:46 +0530

    fix: used extend instead rest param

--- src/table.ts
+++ src/table.ts
@@ -1373,16 +1373,17 @@ class Table extends common.ServiceObject {
       metadata.schema = Table.createSchemaFromString_(metadata.schema);
     }
 
-    metadata = {
-      ...{
+    metadata = extend(
+      true,
+      {
         destinationTable: {
           projectId: this.bigQuery.projectId,
           datasetId: this.dataset.id,
           tableId: this.id,
         },
       },
-      ...metadata,
-    };
+      metadata
+    );
 
     let jobId = metadata.jobId || uuid.v4();
 
