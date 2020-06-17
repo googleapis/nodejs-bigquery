de1998b1c9e93d528a30396f81cd019c0d514e1c
steffnay <steffannbrown@gmail.com>
2020-06-16 19:00:10 -0700

    chore: fix lint

--- system-test/bigquery.ts
+++ system-test/bigquery.ts
@@ -102,11 +102,13 @@ describe('BigQuery', () => {
 
   before(async () => {
     // Remove buckets created for the tests.
-    await deleteBuckets(),
-      // Remove datasets created for the tests.
-      await deleteDatasets(),
-      // Create the test dataset with a label tagging this as a test run.
-      await dataset.create({labels: {[GCLOUD_TESTS_PREFIX]: ''}});
+    await deleteBuckets();
+
+    // Remove datasets created for the tests.
+    await deleteDatasets();
+
+    // Create the test dataset with a label tagging this as a test run.
+    await dataset.create({labels: {[GCLOUD_TESTS_PREFIX]: ''}});
 
     // Create the test table.
     await table.create({schema: SCHEMA});
