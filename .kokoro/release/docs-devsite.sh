b8e7ff38eff1021e28e80770abb79ed1099abbba
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2021-01-12 19:44:21 +0100

    Fix bigquery in docs

--- .kokoro/release/docs-devsite.sh
+++ .kokoro/release/docs-devsite.sh
@@ -46,11 +46,11 @@ sed -i -e '/name: I[A-Z]/{N;d;}' ./yaml/toc.yml
 sed -i -e '/^ *\@google-cloud.*:interface/d' ./yaml/toc.yml
 
 sed -i -e '4i\
-\ \ \ \ summary: Dialogflow.
+\ \ \ \ summary: Bigquery.
 ' ./yaml/toc.yml
 
 sed -i -e '5i\
-\ \ \ \ description: Client library for Dialogflow.
+\ \ \ \ description: Client library for Bigquery.
 ' ./yaml/toc.yml
 
 cp ./yaml/toc.yml ./_devsite/toc.yml
