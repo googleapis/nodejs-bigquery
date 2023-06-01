f40b51e13c86bae80ccdb78d53f9cefdf8d785b9
Alvaro Viebrantz <aviebrantz@google.com>
2023-06-01 14:53:32 -0400

    test: parallel tests can error on already deleted dataset

--- samples/test/datasets.test.js
+++ samples/test/datasets.test.js
@@ -174,16 +174,16 @@ describe('Datasets', () => {
     );
 
     for (const dataset of datasets) {
-      const [metadata] = await dataset.getMetadata();
-      const creationTime = Number(metadata.creationTime);
+      try {
+        const [metadata] = await dataset.getMetadata();
+        const creationTime = Number(metadata.creationTime);
 
-      if (isResourceStale(creationTime)) {
-        try {
+        if (isResourceStale(creationTime)) {
           await dataset.delete({force: true});
-        } catch (e) {
-          console.log(`dataset(${dataset.id}).delete() failed`);
-          console.log(e);
         }
+      } catch (e) {
+        console.log(`dataset(${dataset.id}).delete() failed`);
+        console.log(e);
       }
     }
   }
