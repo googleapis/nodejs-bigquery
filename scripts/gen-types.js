92e080dcabae96f2eae71506adc16a68c9899fc5
Alvaro Viebrantz <aviebrantz@google.com>
2024-02-13 13:44:09 -0400

    fix: license year

--- scripts/gen-types.js
+++ scripts/gen-types.js
@@ -20,7 +20,7 @@ const {promisify} = require('util');
 
 const writeFile = promisify(fs.writeFile);
 
-const LICENSE = `// Copyright 2023 Google LLC
+const LICENSE = `// Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
