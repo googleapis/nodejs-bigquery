45a029757d33f6588095cf26adb3ab9b08d5ebb3
Alvaro Viebrantz <aviebrantz@google.com>
2024-06-05 13:53:28 -0400

    ci: remove hide_output

--- owlbot.py
+++ owlbot.py
@@ -18,8 +18,8 @@ import synthtool.languages.node as node
 from synthtool import shell
 
 # Regenerate Discovery types.
-shell.run(('npm','install'), hide_output = True)
-shell.run(('npm','run','types'), hide_output = True)
+shell.run(('npm','install'))
+shell.run(('npm','run','types'))
 node.fix()
 
 node.owlbot_main(templates_excludes=['.github/bug-report.md'])
