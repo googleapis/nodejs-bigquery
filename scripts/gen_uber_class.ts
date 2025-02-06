45f9e6f4c5778a577c664b724fe6d751529d5524
Leah Cole <coleleah@google.com>
2025-02-06 11:36:30 -0500

    re-add options passing

--- scripts/gen_uber_class.ts
+++ scripts/gen_uber_class.ts
@@ -155,8 +155,10 @@ function makeImports(clients) {
   }
 
   imports = imports.concat('} from ".";\n');
+  imports = imports.concat('import type * as gax from "google-gax";\n');
+
   imports = imports.concat(
-    'import {Callback, CallOptions, PaginationCallback} from "google-gax";\n'
+    'import {Callback, CallOptions, ClientOptions, PaginationCallback} from "google-gax";\n'
   );
   return imports;
 }
@@ -165,30 +167,98 @@ function makeImports(clients) {
 function parseClientName(client) {
   return client.split('ServiceClient')[0].toLowerCase() + 'Client';
 }
-
+function buildOptionTypes(clients) {
+  let output = '';
+  const docstring = `/**
+   * Options passed to the underlying client.
+   *
+   * @param {object} [options] - The configuration object.
+   * The options accepted by the constructor are described in detail
+   * in [this document](https://github.com/googleapis/gax-nodejs/blob/main/client-libraries.md#creating-the-client-instance).
+   * The common options are:
+   * @param {object} [options.credentials] - Credentials object.
+   * @param {string} [options.credentials.client_email]
+   * @param {string} [options.credentials.private_key]
+   * @param {string} [options.email] - Account email address. Required when
+   *     using a .pem or .p12 keyFilename.
+   * @param {string} [options.keyFilename] - Full path to the a .json, .pem, or
+   *     .p12 key downloaded from the Google Developers Console. If you provide
+   *     a path to a JSON file, the projectId option below is not necessary.
+   *     NOTE: .pem and .p12 require you to specify options.email as well.
+   * @param {number} [options.port] - The port on which to connect to
+   *     the remote host.
+   * @param {string} [options.projectId] - The project ID from the Google
+   *     Developer's Console, e.g. 'grape-spaceship-123'. We will also check
+   *     the environment variable GCLOUD_PROJECT for your project ID. If your
+   *     app is running in an environment which supports
+   *     {@link https://developers.google.com/identity/protocols/application-default-credentials Application Default Credentials},
+   *     your project ID will be detected automatically.
+   * @param {string} [options.apiEndpoint] - The domain name of the
+   *     API remote host.
+   * @param {gax.ClientConfig} [options.clientConfig] - Client configuration override.
+   *     Follows the structure of {@link gapicConfig}.
+   * @param {boolean} [options.fallback] - Use HTTP/1.1 REST mode.
+   *     For more information, please check the
+   *     {@link https://github.com/googleapis/gax-nodejs/blob/main/client-libraries.md#http11-rest-api-mode documentation}.
+   * @param {gax} [gaxInstance]: loaded instance of \`google-gax\`. Useful if you
+   *     need to avoid loading the default gRPC version and want to use the fallback
+   *     HTTP implementation. Load only fallback version and pass it to the constructor:
+   *     \`\`\`
+   *     const gax = require('google-gax/build/src/fallback'); // avoids loading google-gax with gRPC
+   *     const client = new DatasetServiceClient({fallback: true}, gax);
+   *     \`\`\`
+   */\n`;
+  const subClientOptionsType = `export type subClientOptions = {opts?: ClientOptions,
+    gaxInstance?: typeof gax | typeof gax.fallback};\n\n`;
+  output = output.concat(docstring, subClientOptionsType);
+  let bigQueryOptionsType = 'export type bigQueryClientOptions = {\n';
+  for (const client in clients) {
+    let variableDecl = '';
+    const clientName = parseClientName(clients[client]);
+    variableDecl = variableDecl.concat(
+      `\t${clientName}?: ${clients[client]};\n`
+    );
+    bigQueryOptionsType = bigQueryOptionsType.concat(variableDecl);
+  }
+  bigQueryOptionsType = bigQueryOptionsType.concat('};\n\n');
+  output = output.concat(bigQueryOptionsType);
+  return output;
+}
 // TODO modify to be able to pass option to subclients
 function buildClientConstructor(clients) {
   let variableDecl = '';
-  // TODO - deal with any, which has to do with options passing
-  let constructorInitializers = '\tconstructor(options:any){\n';
+  const comment = `\t/**
+  * @param {object} [bigQueryClientOptions] - Enables user to instantiate clients separately and use those as the subclients.
+  * @param {object} [subClientOptions] - These options will be shared across subclients. 
+  * To have sub-clients with different options, instantiate each client separately.
+  */`;
+  let constructorInitializers =
+    '\tconstructor(options: bigQueryClientOptions, subClientOptions: subClientOptions){\n';
   for (const client in clients) {
     const clientName = parseClientName(clients[client]);
     variableDecl = variableDecl.concat(
       `\t${clientName}: ${clients[client]};\n`
     );
     constructorInitializers = constructorInitializers.concat(
-      `\t\tthis.${clientName} = options?.${clientName} ?? new ${clients[client]}();\n`
+      `\t\tthis.${clientName} = options?.${clientName} ?? new ${clients[client]}(subClientOptions?.opts, subClientOptions?.gaxInstance);\n`
     );
   }
   constructorInitializers = constructorInitializers.concat('\t}');
   let output = 'export class BigQueryClient{\n';
-  output = output.concat(variableDecl, '\n', constructorInitializers);
+  output = output.concat(
+    variableDecl,
+    '\n',
+    comment,
+    '\n',
+    constructorInitializers
+  );
   return output;
 }
 
 function buildOutput() {
   let newoutput;
   newoutput = output.concat(makeImports(clients));
+  newoutput = newoutput.concat(buildOptionTypes(clients));
   newoutput = newoutput.concat(buildClientConstructor(clients));
   newoutput = newoutput.concat(astHelper(files, clients));
   newoutput = newoutput.concat('\n}');
