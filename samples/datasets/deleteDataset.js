cbcb205a6911d403a60c4ce36cbe10af565332d0
Leah Cole <coleleah@google.com>
2025-07-02 11:46:46 -0700

    add deleteDataset sample

---
+++ samples/datasets/deleteDataset.js
@@ -0,0 +1,68 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+// TODO(coleleah): add a test
+// TODO(coleleah): manually test
+
+async function main(projectId, datasetId) {
+  console.log('executing delete file')
+  // [START bigquery_delete_dataset]
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   */
+  // const projectId = 'YOUR_PROJECT_ID';
+  // const datasetId = 'YOUR_DATASET_ID';
+
+  // Imports the Google Cloud client library
+  const {BigQueryClient} = require('@google-cloud/bigquery');
+
+  // Creates a client
+  const bigqueryClient = new BigQueryClient();
+
+  async function deleteDataset() {
+    // Construct the request object.
+    const request = {
+      projectId: projectId,
+      datasetId: datasetId,
+      deleteContents: true, // Set to true to delete all tables in the dataset
+    };
+
+    try {
+      // Make the API request.
+      console.log('in try block')
+      await bigqueryClient.deleteDataset(request);
+      console.log(`Dataset ${datasetId} deleted.`);
+    } catch (err) {
+      console.error('ERROR deleting dataset:', err);
+      if (err.errors) {
+        err.errors.forEach(e => console.error(e.message));
+      }
+    }
+  }
+
+  await deleteDataset();
+  // [END bigquery_delete_dataset]
+}
+
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+
+if (process.argv.length < 4) {
+  console.error('Usage: node deleteDataset.js <projectId> <datasetId>');
+  process.exitCode = 1;
+}
+main(...process.argv.slice(2)).catch(console.error);
