e914ea7d61a58385bbc5384e9660b8679a1ada70
Leah E. Cole <6719667+leahecole@users.noreply.github.com>
2025-07-22 11:04:08 -0400

    feat: handwritten samples for autogen dataset client (#1496)
    * chore: fix owlbot configs, regenerate
    
    * Apply suggestions from code review
    
    * WIP - two different ways
    
    * don't overwrite handwritten methods
    
    * change to sync file read
    
    * chore: periodic regen
    
    * run postprocessor
    
    * upgrade dependencies to node 18
    
    * Revert "upgrade dependencies to node 18"
    
    This reverts commit b7ca3fc2a68c784cc79beb95289015a0a21ecf65.
    
    * periodic regeneration + update to node 18 on autogen branch
    
    * fix lint errors
    
    * more lint fixes
    
    * migrate bigquery_create_dataset
    
    * modify jules generated sample
    
    * add datasets samples test
    
    * WIP: create dataset test
    
    * reorganize test
    
    * delete dataset test
    
    * add deleteDataset sample
    
    * small cleanup
    
    * add samples and tests for get/list dataset
    
    * WIP: update dataset description
    
    * WIP: update datasets test
    
    * add expiration sample and test
    
    * remove tests that are out of scope for now
    
    * remove todos, add samples-test command
    
    * add prelint session, run fix
    
    * remove leftover old dependency
    
    * add _preview suffix
    
    * add samples-test CI session
    
    * fix region tags
    
    * remove GHA CI samples-tests
    
    * Apply suggestions from code review
    
    make less fun

---
+++ samples/datasets/listDatasets.js
@@ -0,0 +1,65 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+async function main(projectId) {
+  // [START bigquery_list_datasets_preview]
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   */
+  // const projectId = 'YOUR_PROJECT_ID'; // Optional: Defaults to the project ID of the client
+
+  // Imports the Google Cloud client library
+  const {BigQueryClient} = require('@google-cloud/bigquery');
+
+  // Creates a client
+  const bigqueryClient = new BigQueryClient();
+
+  async function listDatasets() {
+    // Construct the request object.
+    const request = {
+      projectId: projectId,
+    };
+
+    try {
+      // Make the API request.
+      const [datasets] = await bigqueryClient.listDatasets(request);
+      console.log('Datasets:');
+      if (datasets.length === 0) {
+        console.log('No datasets found.');
+      } else {
+        datasets.forEach(dataset => console.log(`- ${dataset.id}`));
+      }
+    } catch (err) {
+      console.error('ERROR listing datasets:', err);
+      if (err.errors) {
+        err.errors.forEach(e => console.error(e.message));
+      }
+    }
+  }
+
+  await listDatasets();
+  // [END bigquery_list_datasets_preview]
+}
+
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+
+if (process.argv.length < 3) {
+  console.log('Usage: node listDatasets.js [projectId]');
+}
+main(...process.argv.slice(2)).catch(console.error);
---
+++ samples/datasets/updateDataset.js
@@ -0,0 +1,122 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+async function main(projectId, datasetId, updateOption) {
+  // [START bigquery_update_dataset_description_preview]
+  // [START bigquery_update_dataset_expiration_preview]
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   */
+  // const projectId = 'YOUR_PROJECT_ID'; // Optional: Defaults to the project ID of the client
+
+  // Imports the Google Cloud client library
+  const {BigQueryClient} = require('@google-cloud/bigquery');
+
+  // Creates a client
+  const bigqueryClient = new BigQueryClient();
+  // [END bigquery_update_dataset_expiration_preview]
+
+  async function updateDatasetDescription() {
+    const description = 'This is a new description';
+    const datasetToUpdate = {
+      projectId: projectId,
+      datasetId: datasetId,
+      datasetReference: {
+        datasetId: datasetId,
+      },
+      description: {value: description},
+    };
+    // Construct the request object.
+    const request = {
+      projectId: projectId,
+      datasetId: datasetId,
+      dataset: datasetToUpdate,
+    };
+
+    try {
+      // Make the API request.
+      const [response] = await bigqueryClient.updateDataset(request);
+      console.log(
+        `Dataset ${response.id} description: ${response.description.value}`,
+      );
+    } catch (err) {
+      console.error('ERROR updating dataset:', err);
+      if (err.errors) {
+        err.errors.forEach(e => console.error(e.message));
+      }
+    }
+  }
+  // [END bigquery_update_dataset_description_preview]
+
+  // [START bigquery_update_dataset_expiration_preview]
+
+  async function updateDatasetExpiration() {
+    const expirationTime = 24 * 60 * 60 * 1000;
+    const datasetToUpdate = {
+      projectId: projectId,
+      datasetId: datasetId,
+      datasetReference: {
+        datasetId: datasetId,
+      },
+      defaultTableExpirationMs: {value: expirationTime},
+    };
+    // Construct the request object.
+    const request = {
+      projectId: projectId,
+      datasetId: datasetId,
+      dataset: datasetToUpdate,
+    };
+
+    try {
+      // Make the API request.
+      const [response] = await bigqueryClient.updateDataset(request);
+      console.log(
+        `Dataset ${response.id} expiration: ${response.defaultTableExpirationMs.value}`,
+      );
+    } catch (err) {
+      console.error('ERROR updating dataset:', err);
+      if (err.errors) {
+        err.errors.forEach(e => console.error(e.message));
+      }
+    }
+  }
+  // [END bigquery_update_dataset_expiration_preview]
+
+  switch (updateOption) {
+    case 'description':
+      await updateDatasetDescription();
+      break;
+    case 'expiration':
+      await updateDatasetExpiration();
+      break;
+    default:
+      console.log(
+        'Invalid update option - please choose "description" or "expiration"',
+      );
+  }
+}
+
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+
+if (process.argv.length < 3) {
+  console.log(
+    'Usage: node updateDataset.js <projectId> <datasetId> <updateOption>',
+  );
+}
+main(...process.argv.slice(2)).catch(console.error);
